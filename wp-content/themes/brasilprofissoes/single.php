9e6f3fd19cef4fce4b79620e98529741920ae394
Wesley Sousa <wesley@viru.ga>
2014-07-23 17:37:46 -0300

    single de noticias
    Lateral - Mais lidas.

--- wp-content/themes/brasilprofissoes/single.php
+++ wp-content/themes/brasilprofissoes/single.php
@@ -32,24 +32,38 @@
             <section>
                 <div class="container">
                     <div class="row">
-                    <br>
-                    <ol class="breadcrumb">
-                        <li><a href="<?php echo home_url(); ?>">Home</a></li>
-                        <li><a href="<?php echo home_url(); ?>/?cat=24">Notícias</a></li>
-                        <li class="active"><?php the_title(); ?></li>
-                    </ol>
-                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                            <article>
-                                <?php the_title(); ?>
-                                <?php the_content(); ?>
-                            </article>
-                        <?php endwhile?>
-                        <?php else: ?>
-                            <article>
-                                <h2>Nada Encontrado</h2>
-                                <p>Lamentamos mas não foram encontrados artigos.</p>
-                            </article>            
-                        <?php endif; ?>
+                        <div class="col-md-9">
+                            <br>
+                            <ol class="breadcrumb">
+                                <li><a href="<?php echo home_url(); ?>">Home</a></li>
+                                <li><a href="<?php echo home_url(); ?>/?cat=24">Notícias</a></li>
+                                <li class="active"><?php the_title(); ?></li>
+                            </ol>
+                            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                                <article>
+                                    <?php the_title(); ?>
+                                    <?php the_content(); ?>
+                                </article>
+                            <?php endwhile?>
+                            <?php else: ?>
+                                <article>
+                                    <h2>Nada Encontrado</h2>
+                                    <p>Lamentamos mas não foram encontrados artigos.</p>
+                                </article>            
+                            <?php endif; ?>
+                        </div>
+                        <div class="col-md-3 nopad">
+                            <div class="mais-lidas">
+                                <h3>Mais lidas</h3>
+                                <ol>
+                                    <li><a href="noticia.php">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</a></li>
+                                    <li><a href="noticia.php">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</a></li>
+                                    <li><a href="noticia.php">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</a></li>
+                                    <li><a href="noticia.php">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</a></li>
+                                    <li><a href="noticia.php">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</a></li>
+                                </ol>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
