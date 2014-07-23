ed7f11e33bbd5a0fc7798ad283e297970ffc4e8d
Wesley Sousa <wesley@viru.ga>
2014-07-23 19:14:30 -0300

    Read more em Cat-noticias

--- wp-content/themes/brasilprofissoes/category.php
+++ wp-content/themes/brasilprofissoes/category.php
@@ -4,8 +4,8 @@
         <div class="container">
         	<div class="row">
             	<br>
-                <h2 class="col-md-12"><?php single_cat_title( '', true ); ?></h2>
         		<div class="col-md-9 col-sm-8 noticias">
+                    <h2><?php single_cat_title( '', true ); ?></h2>
                     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             			<article>
                             <a href="<?php the_permalink() ?>">
@@ -13,6 +13,7 @@
                             </a>
                             <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
                             <?php the_excerpt(); ?>
+                            <a class="readmore" href="<?php the_permalink() ?>">mais</a>
                         </article>
                     <?php endwhile?>
                         <?php paginate(); ?>
