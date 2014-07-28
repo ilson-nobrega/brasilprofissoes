b92def066ab762be3a40f8318419a4ca1512e11a
Alexandre Chinaider Moreira <chinaider_ale@live.com>
2014-07-28 17:30:33 -0300

    Pagina termos e politica pronta

---
+++ wp-content/themes/brasilprofissoes/page-termos-e-politica.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+    
+    <section>
+        <div class="container">
+            <div class="row politica-inicio">
+            	<div class="col-md-9 col-sm-9">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/img/termos.jpg" class="img-responsive">
+                </div>
+                <div class="col-md-3 nopad col-sm-3">
+                    <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/img/estado.jpg" class="img-responsive"></a>
+                </div>
+                <div class="col-md-10 col-md-offset-1 col-sm-11 pad-y">
+                   <p class="texto-termos text-justify">Esta política de privacidade estabelece a forma como Brasil Profissões usa e protege todas as informações que você fornecer no Brasil Profissões quando você usa este site. Brasil Profissões está empenhada em assegurar que sua privacidade é protegida. Nós estamos solicitando para você fornecer algumas informações através do qual você pode ser identificado quando se usa este site, então você pode ter certeza de que ela será usada somente em conformidade com esta declaração de privacidade. Brasil Profissões pode alterar esta política de tempos em tempos, atualizando esta página. Você deve verificar esta página de vez em quando para garantir que você esteja feliz com todas as mudanças. Esta política está em vigor desde de 2007.</p> 
+                </div>
+            </div>
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-verde">
+                    <h1 class="col-md-offset-1 col-md-10">O que nós coletamos</h1>
+                </div>
+                
+                    <div class="texto-termos pad-y col-md-10 col-md-offset-1">
+                    <p>Podemos recolher as seguintes informações:</p>
+                        <ul class="lista_termos texto-termos">
+                            <li>Nome e cargo;</li>
+                            <li>informações de Contato, incluindo o seu endereço de e-mail;</li>
+                            <li>Informação demográfica, tais como código postal, preferências e interesses;</li>
+                            <li>Outras informações relevantes para clientes e pesquisas e/ou ofertas.</li>
+                        </ul>
+                    </div>
+
+            </div>
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-amarelo">
+                    <h1 class="col-md-offset-1 col-md-10">O que fazemos com a informação é coletada</h1>
+                </div>
+
+                    <div class="texto-termos pad-y col-md-10 col-md-offset-1">
+                    <p>Exigimos essas informações para compreender as suas necessidades e oferecer-lhe um melhor serviço e, em especial, pelas seguintes razões:</p>
+                        <ul class="lista_termos texto-termos">
+                            <li>Registros Internos.</li>
+                            <li>Podemos usar as informações para melhorar nossos produtos e serviços.</li>
+                            <li>Podemos enviar periodicamente e-mails promocionais sobre novos produtos, ofertas especiais ou outras informações que achamos que você possa estar interessado usando o endereço de e-mail que você forneceu.</li>
+                            <li>De tempos a tempos, poderemos também utilizar as suas informações para contato com você no mercado para fins de investigação. Podemos contactá-lo por e-mail.</li>
+                            <li>Podemos usar as informações para personalizar o site de acordo com seus interesses.</li>
+                        </ul>
+                    </div>
+
+            </div>
+
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-azul">
+                    <h1 class="col-md-offset-1 col-md-10">Segurança</h1>
+                </div>
+                <div class="texto-termos col-md-10 col-md-offset-1 paragrafo-termos">
+                    <p class="text-justify">Estamos empenhados em garantir que a sua informação esteja segura. A fim de prevenir o acesso não autorizado ou revelação de dados confidenciais, físicos, eletrônicos e gerenciais de procedimentos, nós tentamos assegurar que as informações que coletamos estejam corretamente armazenadas e seguras.</p>
+                </div>
+            </div>
+
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-verde">
+                    <h1 class="col-md-offset-1 col-md-10">Como usamos os cookies</h1>
+                </div>
+                <div class="texto-termos col-md-10 col-md-offset-1 paragrafo-termos">
+                    <p class="text-justify">Um cookie é um pequeno arquivo que pede autorização para ser colocado no disco rígido do computador. Depois de aceitar isto, o arquivo é adicionado e o cookie ajuda a analisar o tráfego na web ou deixa você saber quando você visita um site específico. Cookies permitem que aplicações web possam responder a você como um indivíduo. A aplicação web pode adaptar as suas operações de acordo com suas necessidades, gostos e aversões pelo recolhimento e assim o sistema poderá lembrar de informações sobre as suas preferências.</p>
+
+                    <p class="text-justify">Nós usamos cookies para registrar tráfego e identificar quais páginas estão sendo usadas e também para autenticar você como um membro registrado. Isso nos ajuda a analisar os dados sobre a página de tráfego na web e melhorar o nosso site, a fim de ajustá-lo às necessidades de nossos clientes. Nós só iremos usar essas informações para fins de análise estatística e, em seguida, os dados são removidos do sistema. De um modo geral, os cookies nos ajudam a fornecer-lhe uma site melhor. Também nos permite acompanhar as páginas que você você acha útil e as páginas que você não precisa. Um cookie não nos dá acesso ao seu computador ou qualquer informação sobre você, apenas os dados que você escolheu para partilhar conosco.</p>
+
+                    <p class="text-justify">Você pode optar por aceitar ou recusar os cookies. A maioria dos browsers aceita cookies automaticamente, mas você pode modificar o seu navegador normalmente definindo a diminuir os cookies se você preferir. Isso pode impedi-lo de aproveitar plenamente o site porque não irá permitir o acesso a todas as informações do seu registro.</p>
+                </div>
+            </div>
+
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-amarelo">
+                    <h1 class="col-md-offset-1 col-md-10">Links para outros sites</h1>
+                </div>
+                <div class="texto-termos col-md-10 col-md-offset-1 paragrafo-termos">
+                    <p class="text-justify">O nosso site poderá conter links para que o consumidor possa visitar outros sites do seu interesse facilmente. No entanto, uma vez que você tenha utilizado esses links para deixar o nosso site, não se esqueça que não temos qualquer controle sobre esse outro site. Portanto, não podemos ser responsáveis pela protecção e privacidade de todas as informações que você fornecer, enquanto visitam esses sites e tais sites não são regidos por esta declaração de privacidade. Você deve ter precaução e olhar para a declaração de privacidade aplicável ao site em questão.</p>
+                </div>
+            </div>
+
+            <div class="row pad-y">
+                <div class="col-md-12 separador separador-azul">
+                    <h1 class="col-md-offset-1 col-md-10">Controle suas informações pessoais</h1>
+                </div>
+                <div class="texto-termos col-md-10 col-md-offset-1 paragrafo-termos">
+                    <p class="text-justify">Você pode optar por restringir a coleta ou uso de suas informações pessoais nas seguintes maneiras:</p>
+
+                    <p class="text-justify">Sempre que são convidados a preencher um formulário no site, procure a caixa que você pode clicar para indicar que você não deseja que as informações possam ser utilizadas por qualquer pessoa para fins comerciais Se tiver acordado anteriormente para que nós possamos utilizar suas informações pessoais para fins comerciais, você pode ainda mudar de idéia a qualquer momento. </p>
+
+                    <p class="text-justify">Você deverá apenas enviar um e-mail para brasilprofissoes@brasilprofissoes.com.br</p>
+
+                    <p class="text-justify">Nós não iremos vender, distribuir ou ceder as suas informações pessoais a terceiros, a menos que tenhamos sua permissão ou se formos obrigados por lei a fazê-lo. Podemos utilizar suas informações pessoais para lhe enviar informações promocionais sobre terceiros, o que nós pensamos que você pode achar interessante se você nos avisar o que você deseja e isto irá aconteça. Você pode pedir detalhes sobre as informações pessoais que nos estamos mantendo sobre você. Uma pequena taxa deverá será paga. </p>
+
+                    <p class="text-justify">Se você quiser uma cópia das informações mantidas por nós, você deverá escrever para brasilprofissoes@brasilprofissoes.com.br.</p>
+
+                    <p class="text-justify">Se você acredita que qualquer informação que estamos a realizar sobre você está incorreta ou incompleta, por favor escreva ou e-mail para nós, o mais rapidamente possível, no endereço acima indicado. Nós iremos prontamente corrigir qualquer informação encontrada ou incorrecta. Pela sua atenção o nosso muito obrigado.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+<?php get_footer(); ?>
