cb7b9faaaa4f0b80b83c33bee0657dfa8732d1ca
Ilson Nóbrega <ilson@inobrega.com.br>
2014-07-31 12:19:11 -0300

    Alteração nas Post-Types

--- wp-content/themes/brasilprofissoes/taxonomy-profissoes.php
+++ wp-content/themes/brasilprofissoes/taxonomy-profissoes.php
@@ -1,23 +1,16 @@
 <?php get_header(); ?>
     
-    <section>
-        <div class="container">
-        	<div class="row">
-        	   <br>
-        		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        			<article>
-                        <?php the_title(); ?>
-                        <?php the_content(); ?>
-                    </article>
-                <?php endwhile?>
-                <?php else: ?>
-                    <article>
-                        <h2>Nada Encontrado</h2>
-                        <p>Lamentamos mas não foram encontrados artigos.</p>
-                    </article>            
-                <?php endif; ?>
-        	</div>
-        </div>
-    </section>
+    <?php 
+    
+    
+    $var = get_the_taxonomies();
+    
+    $textoTratado = substr($var['profissoes'], 12);
+    $textoTratado = str_replace(".", "", $textoTratado);
+    echo '<br><br><br><br>'.$textoTratado;
+    
+    
+    
+    ?>
     
 <?php get_footer(); ?>
