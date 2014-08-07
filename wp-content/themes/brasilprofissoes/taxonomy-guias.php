8ac1880cea1a4d7108a0ea1a1799b6d7847a98ef
Alexandre Chinaider Moreira <chinaider_ale@live.com>
2014-08-07 17:00:48 -0300

    Pagina de Guias pronta

--- wp-content/themes/brasilprofissoes/taxonomy-guias.php
+++ wp-content/themes/brasilprofissoes/taxonomy-guias.php
@@ -1,34 +1,4 @@
 <?php get_header(); ?>
-   <style type="text/css" media="screen">
-   	#menu-menu-guias li{
-	background-color: #0d6cac;
-	width: 100%;
-	text-align: center;
-	padding: 16px;
-	margin-top: 10px;
-}
-
-#menu-menu-guias li:last-child,#menu-menu-guias li:first-child{
-	background-color: #2bb673;
-	cursor: pointer;
-}
-
-#menu-menu-guias li:last-child:hover,#menu-menu-guias li:first-child:hover{
-	background-color:#339966;
-	cursor: pointer;
-}
-
-#menu-menu-guias li:hover{
-	background-color:#0a5c94; 
-	cursor: pointer;
-}
-
-#menu-menu-guias li a{
-	color: #fff;
-	font-size: 18px;
-}
-   </style> 
-    
     <section>
 	    <div class="container">
 		    <div class="row">
@@ -41,13 +11,91 @@
 						<img src="<?php echo get_stylesheet_directory_uri() ?>/img/ad_vertical_micro.jpg" class="img-responsive">
 				    </div>     
 			    </div>
-			    	<div class="col-md-9 noticias-enem">
-						<div class="titulo">
-							<h3>Notícias do ENEM</h3>
+			    	
+						<div class="col-md-9 sobre-guias">
+							<div class="titulo">
+							<h3>Sobre Guias</h3>
+							</div>
+							<h4 class="icone_descricao">DESCRIÇÃO</h4>
+							<p>O Exame Nacional do Ensino Médio (Enem) foi criado em 1998 com o objetivo de avaliar o desempenho do estudante ao fim da educação básica, buscando contribuir para a melhoria da qualidade desse nível de escolaridade.</p>
+							<h4 class="icone_descricao">O QUE É?</h4>
+							<p>A partir de 2009 passou a ser utilizado também como mecanismo de seleção para o ingresso no ensino superior. Foram implementadas mudanças no Exame que contribuem para a democratização das oportunidades de acesso às vagas oferecidas por Instituições Federais de Ensino Superior (IFES), para a mobilidade acadêmica e para induzir a reestruturação dos currículos do ensino médio.
+Respeitando a autonomia das universidades, a utilização dos resultados do Enem para acesso ao ensino superior pode ocorrer como fase única de seleção ou combinado com seus processos seletivos próprios.</p>
+							
 						</div>
-						<dic class="imagem_noticia_enem col-md-6 nopad">
+
+			    	<div class="col-md-9 noticias-enem">
+						
+
+						<div class="titulo">
+							<h3>Notícias de Guias</h3>
+						</div>
+						
+						<dic class="imagem_noticia_enem col-md-6">
 							<img src="<?php echo get_stylesheet_directory_uri() ?>/img/noticias_enem.jpg" class="img-responsive">
 						</dic>
+						
+						<div class="noticia_principal col-md-6">
+							<small>09 de Janeiro de 2014</small>
+								<h1>Notícias de GUIAS</h1>
+								<p class="pad-y">Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem. Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi quam lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing.</p>
+								<p class="text-right"><a href="#" class="readmore">Mais</a></p>
+						</div>
+
+						<div class="col-md-6 pad-y">
+							<div class="noticia-guia">
+								<div class="col-md-6 nopad">	
+									<img src="<?php echo get_stylesheet_directory_uri() ?>/img/enem_noticia_1.jpg" class="img-responsive">
+								</div>
+								<div class="col-md-6 nopad">
+									<h4>Notícias sobre GUIAS <small>09 de janeiro de 2014</small></h4>
+									<p>lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi</p>
+									<p class="text-right"><a href="#" class="readmore">Mais</a></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 pad-y">
+							<div class="noticia-guia">
+								<div class="col-md-6 nopad">	
+									<img src="<?php echo get_stylesheet_directory_uri() ?>/img/enem_noticia_1.jpg" class="img-responsive">
+								</div>
+								<div class="col-md-6 nopad">
+									<h4>Notícias sobre GUIAS <small>09 de janeiro de 2014</small></h4>
+									<p>lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi</p>
+									<p class="text-right"><a href="#" class="readmore">Mais</a></p>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="col-md-6 pad-y">
+							<div class="noticia-guia">
+								<div class="col-md-6 nopad">	
+									<img src="<?php echo get_stylesheet_directory_uri() ?>/img/enem_noticia_1.jpg" class="img-responsive">
+								</div>
+								<div class="col-md-6 nopad">
+									<h4>Notícias sobre GUIAS <small>09 de janeiro de 2014</small></h4>
+									<p>lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi</p>
+									<p class="text-right"><a href="#" class="readmore">Mais</a></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 pad-y">
+							<div class="noticia-guia">
+								<div class="col-md-6 nopad">	
+									<img src="<?php echo get_stylesheet_directory_uri() ?>/img/enem_noticia_1.jpg" class="img-responsive">
+								</div>
+								<div class="col-md-6 nopad">
+									<h4>Notícias sobre GUIAS <small>09 de janeiro de 2014</small></h4>
+									<p>lorem vestibulum nec nibh, sollicitudin volutpat at libero litora, non adipiscing. Nulla nunc porta lorem.Wisi</p>
+									<p class="text-right"><a href="#" class="readmore">Mais</a></p>
+								</div>
+							</div>
+						</div>
+
+						
 
 
 			    	</div>
