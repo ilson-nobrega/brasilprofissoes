fd2418f64004d932d46b0a9ce36cc135a654197e
Alexandre Chinaider Moreira <chinaider_ale@live.com>
2014-08-07 14:27:29 -0300

    Inicio pagina do enem

---
+++ wp-content/themes/brasilprofissoes/taxonomy-guias.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+    
+    <section>
+	    <div class="container">
+		    <div class="row">
+		     <br>
+			    <div class="col-md-3 col-sm-4 nopad">
+				    <div class="menu_esquerdo">
+				    	<?php dynamic_sidebar( 'menu-esquerdo' ); ?>
+				    </div>       
+			    </div>
+		    </div>      
+	    </div>
+    </section>
+    
+<?php get_footer(); ?>
