db416e845074ae40cef4532f4e5da7a2352260e7
Alexandre Chinaider Moreira <chinaider_ale@live.com>
2014-08-07 15:15:13 -0300

    Atualuzando pagina Eenem

--- wp-content/themes/brasilprofissoes/taxonomy-guias.php
+++ wp-content/themes/brasilprofissoes/taxonomy-guias.php
@@ -38,9 +38,19 @@
 				    	<?php dynamic_sidebar( 'menu-guias' ); ?>
 				    </div>  
 				    <div class="ad_vertical_micro">
-						<img src="<?php echo get_stylesheet_directory_uri() ?>/img/ad_verticalmicro.jpg" class="img-responsive">
+						<img src="<?php echo get_stylesheet_directory_uri() ?>/img/ad_vertical_micro.jpg" class="img-responsive">
 				    </div>     
 			    </div>
+			    	<div class="col-md-9 noticias-enem">
+						<div class="titulo">
+							<h3>Notícias do ENEM</h3>
+						</div>
+						<dic class="imagem_noticia_enem col-md-6 nopad">
+							<img src="<?php echo get_stylesheet_directory_uri() ?>/img/noticias_enem.jpg" class="img-responsive">
+						</dic>
+
+
+			    	</div>
 		    </div>      
 	    </div>
     </section>
